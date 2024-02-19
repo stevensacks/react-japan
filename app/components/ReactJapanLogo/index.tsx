0dddeeea9e8a620bfd85dc494ad41ef0d4d5fa81
Steven Sacks <stevensacks@gmail.com>
2024-02-19 21:05:22 +0900

    feat: react logo normal size

--- app/components/ReactJapanLogo/index.tsx
+++ app/components/ReactJapanLogo/index.tsx
@@ -11,11 +11,7 @@ const ReactJapanLogo: FC<ReactJapanLogoProps> = () => (
         className="plain-link inline-flex select-none items-center gap-2 font-bold sm:text-2xl"
         to="/"
     >
-        <FontAwesomeIcon
-            className="text-red-700"
-            icon={['fab', 'react']}
-            size="sm"
-        />
+        <FontAwesomeIcon className="text-red-700" icon={['fab', 'react']} />
         <span className="text-grey-900 dark:text-grey-100">React Japan</span>
     </Link>
 );
