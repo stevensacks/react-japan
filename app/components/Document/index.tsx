037aa09c34d56ee47957bda752976c62501a42c6
Steven Sacks <stevensacks@gmail.com>
2024-03-07 18:06:22 +0900

    fix: use absolute url in meta images

--- app/components/Document/index.tsx
+++ app/components/Document/index.tsx
@@ -28,9 +28,18 @@ const Document: FC<DocumentProps> = ({
             />
             <meta content="React Japan" name="og:site_name" />
             <meta content="summary" name="twitter:card" />
-            <meta content="./assets/logo1080.png" name="image" />
-            <meta content="./assets/logo1080.png" name="og:image" />
-            <meta content="./assets/logo1080.png" name="twitter:image" />
+            <meta
+                content="https:/react-japan.dev/assets/logo1080.png"
+                name="image"
+            />
+            <meta
+                content="https:/react-japan.dev/assets/logo1080.png"
+                name="og:image"
+            />
+            <meta
+                content="https:/react-japan.dev/assets/logo1080.png"
+                name="twitter:image"
+            />
             <Meta />
             <Links />
             <ThemeHead isSsrTheme={isSsrTheme} />
