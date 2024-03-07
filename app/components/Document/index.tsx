789007b08f1601ac182b1fbe865eb67f4a527d53
Steven Sacks <stevensacks@gmail.com>
2024-03-07 17:57:36 +0900

    fix: meta image paths

--- app/components/Document/index.tsx
+++ app/components/Document/index.tsx
@@ -28,9 +28,9 @@ const Document: FC<DocumentProps> = ({
             />
             <meta content="React Japan" name="og:site_name" />
             <meta content="summary" name="twitter:card" />
-            <meta content="/assets/logo1080.png" name="image" />
-            <meta content="/assets/logo1080.png" name="og:image" />
-            <meta content="/assets/logo1080.png" name="twitter:image" />
+            <meta content="./assets/logo1080.png" name="image" />
+            <meta content="./assets/logo1080.png" name="og:image" />
+            <meta content="./assets/logo1080.png" name="twitter:image" />
             <Meta />
             <Links />
             <ThemeHead isSsrTheme={isSsrTheme} />
