53945f0381ce6cb2ad697a0f89cb5feb008fc006
Steven Sacks <stevensacks@gmail.com>
2024-03-11 15:20:03 +0900

    feat: update remix dev tools to new version

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import {vitePlugin as remix} from '@remix-run/dev';
-import {remixDevTools} from 'remix-development-tools/vite';
+import {remixDevTools} from 'remix-development-tools';
 import {flatRoutes} from 'remix-flat-routes';
 import {defineConfig} from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
     plugins: [
+        remixDevTools(),
         remix({
             ignoredRouteFiles: ['**/*'],
             routes: async (defineRoutes) =>
@@ -18,7 +19,6 @@ export default defineConfig({
                     ],
                 }),
         }),
-        remixDevTools(),
         tsconfigPaths(),
     ],
     server: {
